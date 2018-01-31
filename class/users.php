e6e2c271eed64d8fd8356e67d73ad7a21d1d4b98
Gerhard H <hoogge@scalda.nl>
2018-01-31 10:22:25 +0100

    Users class
    Added user func

---
+++ class/users.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of users
+ *
+ * @author gthoogendoorn
+ */
+class users {
+    //put your code here
+    public function __construct() {
+        " Test another Hello World";
+    }
+}
